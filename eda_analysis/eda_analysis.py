d02f2d449ae5fb9a3c7a0b9326f9aa27f616e270
James Liu <jamesliu1991@gmail.com>
2020-02-28 15:46:04 -0800

    written docstring and stub for describe_na_values

---
+++ eda_analysis/eda_analysis.py
@@ -0,0 +1,14 @@
+def describe_na_values(dataframe):
+    '''
+    describes the na_values in an input pandas dataframe as a 2d array of 1's and 0's.
+    Parameters
+    ----------
+    dataframe
+        the input pd.DataFrame object.
+
+    Returns
+    -------
+        A 2d Numpy Array of 1's and 0's, corresponding to the value of each entry in the dataframe.
+            0 represents an NA value, 1 represents a non-NA value.
+    '''
+    pass
