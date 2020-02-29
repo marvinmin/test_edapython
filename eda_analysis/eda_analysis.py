2acfc7c05ba16ad644b20477f4afae2e218a6556
James Liu <jamesliu1991@gmail.com>
2020-02-29 08:58:45 -0800

    updated param and return type objects in accordance to scipy style

--- eda_analysis/eda_analysis.py
+++ eda_analysis/eda_analysis.py
@@ -3,12 +3,13 @@ def describe_na_values(dataframe):
     describes the na_values in an input pandas dataframe as a 2d array of 1's and 0's.
     Parameters
     ----------
-    dataframe
+    dataframe: Pandas.DataFrame
         the input pd.DataFrame object.
 
     Returns
     -------
+    numpy.ndarray
         A 2d Numpy Array of 1's and 0's, corresponding to the value of each entry in the dataframe.
-            0 represents an NA value, 1 represents a non-NA value.
+        0 represents an NA value, 1 represents a non-NA value.
     '''
     pass
