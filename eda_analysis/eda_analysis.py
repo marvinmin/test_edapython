c5b09d971fcdb63dcb901edf40e7cdb0057121b0
Gaurav Sinha <51436979+sgauravm@users.noreply.github.com>
2020-03-06 15:05:43 -0800

    Update eda_analysis/eda_analysis.py
    Co-Authored-By: marvinmin <55464330+marvinmin@users.noreply.github.com>

--- eda_analysis/eda_analysis.py
+++ eda_analysis/eda_analysis.py
@@ -93,14 +93,13 @@ def describe_cat_var(dataframe,cat_vars, n_cols = 3):
     if not isinstance(cat_vars,list) or not all(isinstance(x, str) for x in cat_vars):
         raise Exception("The value of the argument 'cat_vars' must be a list of strings")
     
-    dataframe=dataframe.dropna()
     col_set = set(dataframe.columns)
     col_subset = set(cat_vars)
     if not col_subset.issubset(col_set):
         raise Exception("The input categorical column names must belong to the dataframe")
     
-
-    data = dataframe[cat_vars]
+    dataframe=dataframe.dropna()
+    data = dataframe[col_subset]
     n = len(cat_vars)
     n_cols = n_cols
     n_rows = int(np.ceil(n/n_cols))
