3d78aafdf0b10b46669e2e9f4873f4007708c8f8
James Liu <jamesliu1991@gmail.com>
2020-03-09 19:08:46 -0700

    flake8 changes to enforce pep8

--- src/eda_analysis/eda_analysis.py
+++ src/eda_analysis/eda_analysis.py
@@ -7,9 +7,10 @@ import pandas as pd
 
 def generate_report(dataframe, cat_vars, num_vars):
     """
-    This function generates an EDA report by plotting graphs and tables for the 
-    numeric variables, categorical variables, NA values and correlation in a dataframe
-    
+    This function generates an EDA report by plotting graphs and tables for the
+    numeric variables, categorical variables, NA values and correlation
+    in a dataframe
+
     Parameters:
     -----------
     dataframe: pandas.DataFrame
@@ -18,12 +19,12 @@ def generate_report(dataframe, cat_vars, num_vars):
         A list containing names of categorical variables
     num_vars: list
         A list containing names of numerical variable
-        
+
     Returns:
     --------
     boolean
         It returns True on successful execution else returns False
-        
+
     Examples:
     ---------
     >>> X= pandas.DataFrame({
@@ -33,12 +34,13 @@ def generate_report(dataframe, cat_vars, num_vars):
     >>>cat_vars = ['type']
     >>>num_vars = ['height']
     >>> describe_cat_variable(X,cat_vars,num_vars)
-    
+
     """
     try:
         na_df = describe_na_values(dataframe)
         na_data = na_df.values
-        na_report = pd.DataFrame(data=np.sum(na_data != 1, axis=1), index=list(dataframe.columns))
+        na_report = pd.DataFrame(data=np.sum(na_data != 1, axis=1),
+                                 index=list(dataframe.columns))
         print("Number of NA values in each column")
         print(na_report)
 
@@ -71,7 +73,8 @@ def generate_report(dataframe, cat_vars, num_vars):
 
 
 def describe_na_values(dataframe: pd.DataFrame):
-    """describes the na_values in an input pandas dataframe as a 2d array of 1's and 0's.
+    """describes the na_values in an input pandas dataframe
+     as a 2d array of 1's and 0's.
 
     Parameters
     ----------
@@ -81,7 +84,8 @@ def describe_na_values(dataframe: pd.DataFrame):
     Returns
     -------
     numpy.ndarray
-        A 2d Numpy Array of 1's and 0's, corresponding to the value of each entry in the dataframe.
+        A 2d Numpy Array of 1's and 0's, corresponding to the value
+        of each entry in the dataframe.
         0 represents an NA value, 1 represents a non-NA value.
 
     Examples:
@@ -120,15 +124,16 @@ def describe_na_values(dataframe: pd.DataFrame):
         raise Exception("the input data is not a dataframe.")
 
     bool_array = dataframe.isna()
-    na_val = np.array([[0 if val else 1 for val in bool_array[col]] for col in bool_array.columns])
+    na_val = np.array([[0 if val else 1 for val in bool_array[col]]
+                       for col in bool_array.columns])
     return pd.DataFrame(data=na_val, index=dataframe.columns)
 
 
 def describe_cat_var(dataframe, cat_vars, n_cols=3):
     """
-    This function will take dataframe and categorical variable names and will 
+    This function will take dataframe and categorical variable names and will
     plot the histogram of each categorical variable.
-    
+
     Parameters:
     -----------
     dataframe: pandas.DataFrame
@@ -137,12 +142,12 @@ def describe_cat_var(dataframe, cat_vars, n_cols=3):
         A list containing names of categorical variables
     n_cols: int (default: 3)
         A number indicating how many plots should be displayed in a row
-    
+
     Returns:
     --------
     altair
         a grid of altair plot containing all histograms
-    
+
     Examples:
     ---------
     >>> X= pandas.DataFrame({
@@ -151,23 +156,28 @@ def describe_cat_var(dataframe, cat_vars, n_cols=3):
     })
     >>> cat_vars = ['type']
     >>> describe_cat_variable(X,cat_vars)
-       
+
     """
 
     # Checking for valid inputs
     if not isinstance(dataframe, pd.DataFrame):
-        raise Exception("The value of the argument 'dataframe' must be of type 'pandas.DataFrame'")
+        raise Exception("The value of the argument 'dataframe' must be " +
+                        "of type 'pandas.DataFrame'")
 
-    if not isinstance(cat_vars, list) or not all(isinstance(x, str) for x in cat_vars):
-        raise Exception("The value of the argument 'cat_vars' must be a list of strings")
+    if not isinstance(cat_vars, list) or \
+            not all(isinstance(x, str) for x in cat_vars):
+        raise Exception("The value of the argument 'cat_vars' must be " +
+                        "a list of strings")
 
     if not isinstance(n_cols, int) or n_cols <= 0:
-        raise Exception("The value of the argument 'n_cols' must be a positive non zero integer")
+        raise Exception("The value of the argument 'n_cols' must be " +
+                        "a positive non zero integer")
 
     col_set = set(dataframe.columns)
     col_subset = set(cat_vars)
     if not col_subset.issubset(col_set):
-        raise Exception("The input categorical column names must belong to the dataframe")
+        raise Exception("The input categorical column names must belong to " +
+                        "the dataframe")
 
     dataframe = dataframe.dropna()
     data = dataframe[col_subset]
@@ -186,7 +196,8 @@ def describe_cat_var(dataframe, cat_vars, n_cols=3):
             hist = alt.Chart(data).mark_bar(width=40).encode(
                 x=alt.X(cols + ':O'),
                 y='count()'
-            ).properties(height=200, width=300, title='Histogram of ' + cat_vars[z])
+            ).properties(height=200, width=300, title='Histogram of '
+                                                      + cat_vars[z])
             z = z + 1
             if j == 0:
                 row_plot = hist
@@ -201,8 +212,7 @@ def describe_cat_var(dataframe, cat_vars, n_cols=3):
 
 
 def describe_num_var(dataframe, num_vars):
-    """ 
-    This function takes dataframe and numeric variable names and provides 
+    """This function takes dataframe and numeric variable names and provides
     statistical summary of the numeric variables for a dataframe.
     Also, the function plots the histogram of each numeric variable.
 
@@ -211,8 +221,9 @@ def describe_num_var(dataframe, num_vars):
     dataframe: pandas.DataFrame
         The dataframe to be inspected.
     num_vars: list
-        A list of unique character strings of the names of the numeric variables.
-    
+        A list of unique character strings of the names of
+        the numeric variables.
+
     Returns:
     --------
     tuple(pandas.DataFrame, altair)
@@ -220,7 +231,7 @@ def describe_num_var(dataframe, num_vars):
             statistical summary of the numeric variables
         altair
             a grid of altair plot containing all histograms
-    
+
     Examples:
     ---------
     >>> X= pandas.DataFrame({
@@ -232,30 +243,36 @@ def describe_num_var(dataframe, num_vars):
     >>> summary, plot = describe_num_var(X, num_vars)
     >>> summary
     >>> plot
-      
+
     """
     # Check the dataframe input
     if not isinstance(dataframe, pd.DataFrame):
-        raise Exception("The value of the argument 'dataframe' should be of type pandas dataframe.")
+        raise Exception("The value of the argument 'dataframe' " +
+                        "should be of type pandas dataframe.")
 
     # Check the num_vars input should be a list of strings
-    if not (all(isinstance(item, str) for item in num_vars) & isinstance(num_vars, list)):
-        raise Exception("The value of the argument 'num_vars' should be a list of strings.")
+    if not (all(isinstance(item, str) for item in num_vars) &
+            isinstance(num_vars, list)):
+        raise Exception("The value of the argument 'num_vars' " +
+                        "should be a list of strings.")
 
     # Check if the elements in the num_vars input are unique
     if len(num_vars) != len(set(num_vars)):
-        raise Exception("The elements in the argument 'num_vars' should be unique.")
+        raise Exception("The elements in the argument 'num_vars' " +
+                        "should be unique.")
 
     # Check if the num_vars input contains only the column names
     if not all(item in dataframe.columns for item in num_vars):
-        raise Exception("The argument 'num_vars' should be a subset of the column names from the dataframe.")
+        raise Exception("The argument 'num_vars' should be a subset of" +
+                        " the column names from the dataframe.")
 
-    # Subset and transpose the dataframe for later use 
+    # Subset and transpose the dataframe for later use
     df = pd.DataFrame(dataframe[num_vars]).T
 
     # Check if only the numeric columns are selected
     if not np.issubdtype(df.to_numpy().dtype, np.number):
-        raise Exception("Only numeric columns expected, please check the input.")
+        raise Exception("Only numeric columns expected, please " +
+                        "check the input.")
 
     # Calculate the statistical summaries
     stat_funs = [np.nanmin, np.nanmax, np.nanmedian, np.nanmean, np.nanstd]
@@ -293,7 +310,7 @@ def describe_num_var(dataframe, num_vars):
 
 def calc_cor(dataframe, num_vars):
     """
-    This function evaluates the correlation between the numeric 
+    This function evaluates the correlation between the numeric
     variables of a given dataframe.
 
     Parameters:
@@ -306,7 +323,8 @@ def calc_cor(dataframe, num_vars):
     Returns:
     --------
     altair
-        A correlogram plot labelled with the correlation coefficients 
+
+        A correlogram plot labelled with the correlation coefficients
         of -1 to 1 between each numeric column and other numeric variables
         in the dataframe.
 
@@ -318,19 +336,23 @@ def calc_cor(dataframe, num_vars):
     'height':[10,20,30]
     })
     >>>num_var = ['height', 'width']
-    >>> calc_cor(X, num_vars)    
+    >>> calc_cor(X, num_vars)
     """
+
     # Test input 'dataframe' is a dataframe
     if not isinstance(dataframe, pd.DataFrame):
         raise Exception("Input 'dataframe' is not a dataframe")
 
     # Check the num_vars input should be a list of strings
-    if not (all(isinstance(item, str) for item in num_vars) & isinstance(num_vars, list)):
-        raise Exception("The value of the argument 'num_vars' should be a list of strings.")
+    if not (all(isinstance(item, str) for item in num_vars) &
+            isinstance(num_vars, list)):
+        raise Exception("The value of the argument 'num_vars' " +
+                        "should be a list of strings.")
 
     # Check if the num_vars input contains only the column names
     if not all(item in dataframe.columns for item in num_vars):
-        raise Exception("The argument 'num_vars' should be a subset of the column names from the dataframe.")
+        raise Exception("The argument 'num_vars' should be a subset " +
+                        "of the column names from the dataframe.")
 
     for i in num_vars:
         if not np.issubdtype(dataframe[i].dtype, np.number):
@@ -338,7 +360,8 @@ def calc_cor(dataframe, num_vars):
 
     # Check if the elements in the num_vars input are unique
     if len(num_vars) != len(set(num_vars)):
-        raise Exception("The elements in the argument 'num_vars' should be unique.")
+        raise Exception("The elements in the argument 'num_vars' " +
+                        "should be unique.")
 
     df_num = dataframe.loc[:, num_vars]
     df_num = df_num.dropna()
